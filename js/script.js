f03f94fb21ec136be59d7eae1fa8eeff62a33b52
prasuKalla <201551049@iiitvadodara.ac.in>
2018-12-08 21:48:18 +0530

    first part done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var pics = [
+  "./img/p1.jpg",
+  "./img/p2.jpg",
+  "./img/p3.jpg",
+  "./img/p4.jpg",
+  "./img/p5.jpg"
+];
+
+var curImg= 0;
+
+function rightClick() {
+  curImg = (curImg + 1) % pics.length;
+   document.getElementById("pic").src = pics[curImg];
+}
+
+function leftClick() {
+   if(curImg <= 0 || curImg>pics.length){
+   	curImg=4;
+   }
+	else if(curImg>0 && curImg<=pics.length){
+	  	curImg-=1;
+	  }
+   
+   document.getElementById("pic").src = pics[curImg];
+}
+
+
